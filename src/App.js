0dd0b6274500a429771fd14bbe8e33530ed0449f
Elagoht <furkanbaytekin@gmail.com>
2023-01-10 13:06:24 +0300

    chore: extract data to external file

--- src/App.js
+++ src/App.js
@@ -3,80 +3,9 @@ import { useEffect, useState } from 'react'
 import { SelectedAlbumContext } from './contexts/SelectedAlbumContext'
 import AlbumInfo from './components/AlbumInfo'
 import AllAlbums from './components/AllAlbums'
-import { covers } from './assets/images'
 import Links from './components/Links'
 import Footer from './components/Footer'
-
-const albums = [
-  {
-    title: "Protect Your Roots",
-    artist: "Elàgoht",
-    year: 2019,
-    composer: "Furkan Baytekin",
-    cover: covers.protect,
-    instruments: {
-      "bassguitar": "Furkan Baytekin",
-      "leadguitar": "Furkan Baytekin",
-      "drumkit": "Furkan Baytekin",
-      "keyboard": "Furkan Baytekin",
-      "vocal": "Furkan Baytekin",
-    },
-    links: {
-      youtube: "#",
-      google: "#",
-      spotify: "#",
-      apple: "#",
-      deezer: "#",
-      soundcloud: "#",
-      tidal: "#"
-    }
-  }, {
-    title: "Behind Of War",
-    artist: "Elàgoht",
-    year: 2019,
-    composer: "Furkan Baytekin",
-    cover: covers.behind,
-    instruments: {
-      "vocal": "Furkan Baytekin",
-      "keyboard": "Furkan Baytekin",
-      "rhythmguitar": "Furkan Baytekin",
-      "leadguitar": "Furkan Baytekin",
-      "bassguitar": "Furkan Baytekin",
-      "drumkit": "Furkan Baytekin",
-    },
-    links: {
-      google: "#",
-      youtube: "#",
-      spotify: "#",
-      apple: "#",
-      deezer: "#",
-      soundcloud: "#",
-      tidal: "#"
-    }
-  }, {
-    title: "Demolition Of Obsidian",
-    artist: "Elàgoht",
-    year: 2020,
-    composer: "Furkan Baytekin",
-    cover: covers.demolition,
-    instruments: {
-      "rhythmguitar": "Furkan Baytekin",
-      "keyboard": "Furkan Baytekin",
-      "drumkit": "Furkan Baytekin",
-      "bassguitar": "Furkan Baytekin",
-      "vocal": "Furkan Baytekin",
-    },
-    links: {
-      youtube: "#",
-      spotify: "#",
-      apple: "#",
-      deezer: "#",
-      google: "#",
-      soundcloud: "#",
-      tidal: "#"
-    }
-  }
-]
+import albums from './albumData'
 
 function App() {
 
---
+++ src/albumData.js
@@ -0,0 +1,74 @@
+import { covers } from './assets/images'
+
+const albums = [
+  {
+    title: "Protect Your Roots",
+    artist: "Elàgoht",
+    year: 2019,
+    composer: "Furkan Baytekin",
+    cover: covers.protect,
+    instruments: {
+      "bassguitar": "Furkan Baytekin",
+      "leadguitar": "Furkan Baytekin",
+      "drumkit": "Furkan Baytekin",
+      "keyboard": "Furkan Baytekin",
+      "vocal": "Furkan Baytekin",
+    },
+    links: {
+      youtube: "#",
+      google: "#",
+      spotify: "#",
+      apple: "#",
+      deezer: "#",
+      soundcloud: "#",
+      tidal: "#"
+    }
+  }, {
+    title: "Behind Of War",
+    artist: "Elàgoht",
+    year: 2019,
+    composer: "Furkan Baytekin",
+    cover: covers.behind,
+    instruments: {
+      "vocal": "Furkan Baytekin",
+      "keyboard": "Furkan Baytekin",
+      "rhythmguitar": "Furkan Baytekin",
+      "leadguitar": "Furkan Baytekin",
+      "bassguitar": "Furkan Baytekin",
+      "drumkit": "Furkan Baytekin",
+    },
+    links: {
+      google: "#",
+      youtube: "#",
+      spotify: "#",
+      apple: "#",
+      deezer: "#",
+      soundcloud: "#",
+      tidal: "#"
+    }
+  }, {
+    title: "Demolition Of Obsidian",
+    artist: "Elàgoht",
+    year: 2020,
+    composer: "Furkan Baytekin",
+    cover: covers.demolition,
+    instruments: {
+      "rhythmguitar": "Furkan Baytekin",
+      "keyboard": "Furkan Baytekin",
+      "drumkit": "Furkan Baytekin",
+      "bassguitar": "Furkan Baytekin",
+      "vocal": "Furkan Baytekin",
+    },
+    links: {
+      youtube: "#",
+      spotify: "#",
+      apple: "#",
+      deezer: "#",
+      google: "#",
+      soundcloud: "#",
+      tidal: "#"
+    }
+  }
+]
+
+export default albums
